d9f546186d780a73f786d9310b48fa69b8bdc90a
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-08-24 16:44:12 +0530

    initial

---
+++ controllers/messageControllers.js
@@ -0,0 +1,65 @@
+const asyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+
+//@description     Get all Messages
+//@route           GET /api/Message/:chatId
+//@access          Protected
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name pic email")
+      .populate("chat");
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+//@description     Create New Message
+//@route           POST /api/Message/
+//@access          Protected
+const sendMessage = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request", req.body);
+    return res.status(400).json({ error: "Content or chatId missing" });
+  }
+
+  var newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    var message = await Message.create(newMessage);
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+
+    await Chat.findByIdAndUpdate(chatId, { latestMessage: message });
+
+    res.json(message);
+  } catch (error) {
+    console.error("Error sending message:", error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+const deleteAllMessages = asyncHandler(async (req, res) => {
+  try {
+    await Message.deleteMany({});
+    res.status(200).json({ message: 'All messages have been deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete messages', error: error.message });
+  }
+});
+
+module.exports = { allMessages, sendMessage, deleteAllMessages };
