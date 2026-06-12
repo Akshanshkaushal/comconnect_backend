4d504402c8ce6a42e806ce900f2e433c5ca28044
Akshansh Kaushal <113452367+Akshanshkaushal@users.noreply.github.com>
2026-06-12 11:58:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,8 +45,7 @@ const server = app.listen(
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
+  cors: { 
     // credentials: true,
   },
 });
